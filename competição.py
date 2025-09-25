53458f5c577b5d7e06522a474396fcfea4429c5c
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-25 19:21:21 -0300

    Cabeçalho adicionado

---
+++ competição.py
@@ -0,0 +1,11 @@
+'''
+    Autores: Glauber Almeida de Brito
+    Turma: 2ºA DS               Data: 25/09/2025
+
+    Atividade Competitiva - Glauber X Gustavo dos Santos
+    - Faça um programa que leia o sexo de uma pessoa, 
+      mas só aceite os valores 'M' ou 'F'. Caso esteja errado, 
+      peça a digitação novamente até ter um valor correto
+'''
+
+
